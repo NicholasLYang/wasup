a23442eb0ab2fdde4d43bbc62893d7a318f3aa99
Nicholas Yang <nick@nicholasyang.com>
2021-08-03 19:07:45 -0500

    Cleaning up index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,7 @@
+import { createModule } from './lib/builder';
+import { decodeModule } from './lib/decoder';
 import { encodeModule } from './lib/encoder';
 
 export * from './lib/builder';
 export * from './lib/leb128';
-export { encodeModule };
-import * as fs from 'fs';
-import { decodeModule } from './lib/decoder';
-
-//@ts-ignore
-const wasmCode = fs.readFileSync('examples/example3.wasm');
-const myModule = decodeModule(wasmCode);
-const encodedModule = encodeModule(myModule);
-fs.writeFileSync('rust_example_out.wasm', encodedModule);
+export { encodeModule, decodeModule, createModule };
