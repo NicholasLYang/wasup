9a2dcf848b4e129c9d44a482200a8df027f8a383
Nicholas Yang <nicholas@rome.tools>
2022-05-26 16:58:38 -0500

    Prettier

--- src/lib/printer.ts
+++ src/lib/printer.ts
@@ -250,9 +250,9 @@ export function printInstruction(instr: Instruction): string {
         .join('\n')}end)`;
     }
     case InstrType.If: {
-      const out = `(${instrTypeString} ${printBlockType(
-        instr[1]
-      )} ${instr[2].map(printInstruction).join('\n')}`;
+      const out = `(${instrTypeString} ${printBlockType(instr[1])} ${instr[2]
+        .map(printInstruction)
+        .join('\n')}`;
       if (instr[3]) {
         return `${out} else ${instr[3].map(printInstruction).join('\n')} end)`;
       }
